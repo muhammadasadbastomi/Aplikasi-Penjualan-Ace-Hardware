87b058afd2b01f1e4f1a48684e357d0cf86a2f86
aryudi33 <33aryudi@gmail.com>
2020-05-17 01:26:19 +0800

    Sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -13,22 +13,26 @@
     <div class="shadow-bottom"></div>
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li class=" nav-item"> <a href="{{route('adminIndex')}}"><i class="feather icon-home"></i><span class="menu-title">Dashboard</span></a></li>
-            <li class=" nav-item"><a href="#"><i class="feather icon-server "></i><span class="menu-title">Master
+            <li class="nav-item"> <a href="{{route('adminIndex')}}"><i class="feather icon-home"></i><span class="menu-title">Dashboard</span></a></li>
+            <li class="nav-item"><a href="#"><i class="feather icon-server "></i><span class="menu-title">Master
                         Data</span></a>
                 <ul class="menu-content">
                     <li><a href="{{ route('thumbIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Thumbnail</span></a></li>
                     <li><a href="{{ route('supplierIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Supplier</span></a></li>
                     <li><a href="{{ route('barangIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Barang Master</span></a></li>
+                </ul>
+            </li>
+            <li class="nav-item"><a href="#"><i class="feather icon-folder"></i><span class="menu-title">Pengelolaan</span></a>
+                <ul class="menu-content">
                     <li><a href="{{ route('datangIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Barang Datang</span></a></li>
                     <li><a href="{{ route('pesananIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Barang Pesanan</span></a></li>
-                    <!-- <li><a href="{{ route('diskonIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Barang Diskon</span></a></li> -->
                     <li><a href="{{ route('pengirimanIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Status Pengiriman</span></a></li>
                     <li><a href="{{ route('terjualIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Barang Terjual</span></a></li>
                     <li><a href="{{ route('garansiIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Barang Garansi</span></a></li>
                     <li><a href="{{ route('rusakIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Barang Rusak</span></a></li>
                     <li><a href="{{ route('perbaikanIndex') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="eCommerce">Dalam Perbaikan</span></a></li>
                 </ul>
+            </li>
             <li class="nav-item"><a href="{{route('userEdit')}}"><i class="feather icon-settings"></i><span class="menu-title" data-i18n="Account Settings">Edit Profile</span></a>
             </li>
             <li class="nav-item"><a href="#"><i class="feather icon-user"></i><span class="menu-title" data-i18n="User">User</span></a>
