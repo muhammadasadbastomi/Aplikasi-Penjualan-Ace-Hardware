a3959e8201aaba3c9a4ca71e7346b6ceda65c421
dedovis <dedovis@gmail.com>
2020-09-09 07:52:39 +0800

    update

--- database/migrations/2020_05_10_101923_create_barangs_table.php
+++ database/migrations/2020_05_10_101923_create_barangs_table.php
@@ -23,7 +23,7 @@ class CreateBarangsTable extends Migration
             $table->string('departement');
             $table->string('harga_jual');
             $table->tinyInteger('kategori');
-            $table->string('keterangan')->nullable();
+            $table->text('keterangan')->nullable();
             $table->integer('stok_tersedia');
             $table->integer('diskon')->nullable();
             $table->date('tgl_mulai')->nullable();
